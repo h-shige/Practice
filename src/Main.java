e9d02996ff40da0d2798a782c89b9c5fe66a5f5d
h-shige <h.shigematsu0510@gmail.com>
2023-02-18 18:22:03 +0900

    マージ後テスト

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1回目のcommitテスト");
         System.out.println("editブランチ作成");
+        System.out.println("editをmasterにマージ");
     }
 }
